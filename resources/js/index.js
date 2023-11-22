0c1a77c8c8fff1380233eb040135ecdb730f7342
bruna-nunes <brugnunes@gmail.com>
2023-11-22 20:23:54 -0300

    feat: implement responsiveness

---
+++ resources/js/index.js
@@ -0,0 +1,8 @@
+function toggleMenuHeader() {
+    var navLinks = document.getElementById("nav-menu-links");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
+  }
